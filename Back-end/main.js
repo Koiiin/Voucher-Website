8b4517dc343c188293c8406fcc77c3d19e5398f1
Keywi75 <hoangkhanh300105@gmail.com>
2025-03-20 15:52:00 +0700

    xong dang nhap chua xong dang ki

--- Back-end/main.js
+++ Back-end/main.js
@@ -1,15 +1,19 @@
-const https = require('https')
-const port = 3000
-let express = require("express")
+const https = require('https');
+const port = 3000;
+let express = require("express");
+const cors = require("cors");
+
 require("dotenv").config();
 const connectDB = require("./database/database");
 const authRoutes = require("./Routes/AuthRoutes");
-const Category = require("./Routes/categoryR")
+const Category = require("./Routes/categoryR");
 let app = express()
 
 //ketnoiDB
 connectDB();
 
+// Cho phép tất cả các origin truy cập
+app.use(cors());
 
 //connfig de nhan gia tir tu nguoi dung gui len 
 app.use(express.urlencoded({extended: true}));
--- Back-end/models/user.js
+++ Back-end/models/user.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    //email: { type: String, required: true, unique: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+   
 });
 
 // Tạo model "User" => MongoDB sẽ tạo collection "users" (tên model ở số nhiều)
