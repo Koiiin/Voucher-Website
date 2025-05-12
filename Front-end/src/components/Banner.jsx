62fd02dc7f1324affef55adb6f9b640a246a4ea7
Vh.Koiiin <23520792@gm.uit.edu.vn>
2025-05-12 20:29:53 +0700

    Update Banner

--- Front-end/src/components/Banner.jsx
+++ Front-end/src/components/Banner.jsx
@@ -1,15 +1,17 @@
 import React from "react";
- import "../styles/Banner.css";
- 
- function Banner() {
-   return (
-     <section className="container">
-       <h1>Chia sẻ và trao đổi voucher</h1>
-       <h1>Tiết kiệm với hàng ngàn voucher giảm giá và gift card</h1>
-       <br></br>
-       <button className="banner-btn">Khám phá ngay</button>
-     </section>
-   );
- }
- 
- export default Banner;
+import "../styles/Banner.css";
+
+function Banner() {
+  return (
+    <section className="container">
+      <h1 className="glow-text">🔥 Chia Sẻ & Săn Voucher Cực Chất 🔥</h1>
+      <h1 className="glow-text">💸 Tiết Kiệm Hàng Ngàn Với Voucher Giảm Giá & Gift Card Đỉnh Cao! 💳</h1>
+      <button className="banner-btn">Khám Phá Ngay</button>
+      <div className="sparkle"></div>
+      <div className="sparkle sparkle-2"></div>
+      <div className="sparkle sparkle-3"></div>
+    </section>
+  );
+}
+
+export default Banner;
