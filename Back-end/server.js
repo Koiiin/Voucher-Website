aeabbe4c535d331db81534777706a3c549b35551
Keywi75 <hoangkhanh300105@gmail.com>
2025-05-30 07:52:46 +0700

    deploy5

--- Back-end/server.js
+++ Back-end/server.js
@@ -33,7 +33,7 @@ app.use(session({
     saveUninitialized: false
 }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://voux.id.vn',
     credentials: true
 }));
 app.use(express.urlencoded({extended: true}));
