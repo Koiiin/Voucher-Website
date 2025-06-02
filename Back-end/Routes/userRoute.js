fd3992982648dbe0c153aad847889740305d8d56
Keywi75 <hoangkhanh300105@gmail.com>
2025-06-02 13:11:34 +0700

    version 4

--- Back-end/Routes/userRoute.js
+++ Back-end/Routes/userRoute.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyTokenOnly, verifyTokenAndAdAuth } = require('../controllers/middlewareController');
-
+const voucherController = require('../controllers/voucherController');
 // Admin - Public
 router.get('/', userController.getAllUser);
 router.get('/:id/vouchers', userController.getUserVouchers);
 router.delete('/:id', userController.deleteUser);
 
+router.get('/vouchers', verifyTokenOnly, voucherController.getUserVouchersByUsername);
 // User - Private - cần token
 router.get('/profile', verifyTokenOnly, userController.getUserProfile);
 router.post('/addVoucherToCart', verifyTokenOnly, userController.addVoucherToCart);
